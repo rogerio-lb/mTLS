48b22266da94c4f3d36d199497f1f9e3a6800d58
Rogerio Inacio <rogerio.reaper@gmail.com>
2025-08-27 16:18:47 -0300

    refactor: remove unnecessary code

--- services/polling.go
+++ services/polling.go
@@ -17,52 +17,9 @@ type GetMessageResponse struct {
 	resourceID string
 }
 
-func StartStream(conn *tls.Conn) (*GetMessageResponse, error) {
-	fmt.Println("Starting stream...")
-
-	request := "GET /api/v1/out/52833288/stream/start HTTP/1.2\r\n" +
-		"Host: icom-h.pi.rsfn.net.br\r\n" +
-		//"Accept: multipart/mixed\r\n" +
-		"User-Agent: Go-http-client/1.2\r\n" +
-		"Connection: Close\r\n\r\n"
-
-	_, err := conn.Write([]byte(request))
-	if err != nil {
-		fmt.Printf("Failed to write request: %v\n", err)
-		return nil, err
-	}
-
-	reader := bufio.NewReader(conn)
-	resp, err := http.ReadResponse(reader, nil)
-	if err != nil {
-		fmt.Printf("Failed to parse response: %v\n", err)
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Printf("Failed to read body: %v\n", err)
-		return nil, err
-	}
-
-	fmt.Printf("Status: %s\n", resp.Status)
-	fmt.Printf("Body received (%d bytes)\n", len(body))
-	fmt.Printf("Body: %s\n", body)
-	fmt.Printf("Headers: %v\n", resp.Header)
-
-	fmt.Println("Stream started successfully.")
-
-	return &GetMessageResponse{
-		Message:    string(body),
-		PIPullNext: resp.Header.Get("Pi-Pull-Next"),
-		resourceID: resp.Header.Get("Pi-Resourceid"),
-	}, nil
-}
-
 func GetMessages(conn *tls.Conn, step string) (*GetMessageResponse, error) {
 	if step == "start" {
-		return StartStream(conn)
+		return GetMessage(conn, "")
 	}
 
 	return GetMessage(conn, step)
@@ -71,10 +28,14 @@ func GetMessages(conn *tls.Conn, step string) (*GetMessageResponse, error) {
 func GetMessage(conn *tls.Conn, pullnext string) (*GetMessageResponse, error) {
 	fmt.Println("Starting stream...")
 
+	if pullnext == "" {
+		pullnext = "/api/v1/out/52833288/stream/start"
+	}
+
 	request := "GET " + pullnext + " HTTP/1.2\r\n" +
 		"Host: icom-h.pi.rsfn.net.br\r\n" +
-		//"Accept: multipart/mixed\r\n" +
-		"Accept: application/xml\r\n" +
+		"Accept: multipart/mixed\r\n" +
+		//"Accept: application/xml\r\n" +
 		"Accept-Encoding: gzip\r\n" +
 		"User-Agent: Go-http-client/1.2\r\n" +
 		"Connection: Close\r\n\r\n"
